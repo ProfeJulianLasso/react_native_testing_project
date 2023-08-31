1e75ac262101e4662d401ac308f26b0960b53110
Julian Lasso <julian.lasso@sofka.com.co>
2023-08-31 16:30:27 -0500

    test: :white_check_mark: test inicial para confirmar configuración de Testing Library

--- __tests__/App.test.tsx
+++ __tests__/App.test.tsx
@@ -1,17 +1,8 @@
-/**
- * @format
- */
+import {render, screen} from '@testing-library/react-native';
+import App from '../src/App';
 
-import 'react-native';
-import React from 'react';
-import App from '../App';
-
-// Note: import explicitly to use the types shiped with jest.
-import {it} from '@jest/globals';
-
-// Note: test renderer must be required after react-native.
-import renderer from 'react-test-renderer';
-
-it('renders correctly', () => {
-  renderer.create(<App />);
+test('render app correctly', () => {
+  render(<App />);
+  const text = screen.queryByText('Welcome to React Native');
+  expect(text).toHaveTextContent('Welcome to React Native');
 });
