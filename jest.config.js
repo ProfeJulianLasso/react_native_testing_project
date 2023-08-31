20de88b21ff04124c6ce59feff340b4347feaffe
Julian Lasso <julian.lasso@sofka.com.co>
2023-08-31 14:13:30 -0500

    chore: :heavy_plus_sign: instalación y configuración de Testing Library
    también se configura el tema del coverage

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,16 @@
 module.exports = {
   preset: 'react-native',
+  setupFilesAfterEnv: ['./jest.setup.js'],
+  rootDir: '.',
+  roots: ['<rootDir>/src/', '<rootDir>/__tests__'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
+  collectCoverageFrom: ['**/*.(t|j)s', '**/*.(t|j)sx'],
+  coverageDirectory: './coverage',
+  testEnvironment: 'node',
+  coveragePathIgnorePatterns: [
+    '/node_modules/',
+    '/__tests__/',
+    '/__mocks__/',
+    'index.ts'
+  ],
 };
